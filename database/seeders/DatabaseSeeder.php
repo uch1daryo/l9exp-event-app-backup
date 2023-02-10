9c1a97175b9bbcce12714a7d0e4db09ae5379225
Uchida Ryo <uchidaryo.gm@gmail.com>
2023-02-10 11:04:24 +0900

    feat: 開発用シーダーを追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,23 +2,37 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Event;
+use App\Models\Room;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
-     *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->count(3)->create();
+        Room::factory()->count(3)->create();
+        Event::create([
+            'room_id' => 1,
+            'user_id' => 1,
+            'start_at' => '2023-02-10 10:00:00',
+            'end_at' => '2023-02-10 11:30:00',
+        ]);
+        Event::create([
+            'room_id' => 2,
+            'user_id' => 3,
+            'start_at' => '2023-02-13 15:00:00',
+            'end_at' => '2023-02-13 17:15:00',
+        ]);
+        Event::create([
+            'room_id' => 3,
+            'user_id' => 2,
+            'start_at' => '2023-02-14 08:00:00',
+            'end_at' => '2023-02-14 10:30:00',
+        ]);
     }
 }
