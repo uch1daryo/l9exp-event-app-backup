36c75190ea04aaf9e8660bf533bd80d835e5e847
Uchida Ryo <uchidaryo.gm@gmail.com>
2023-02-10 13:26:07 +0900

    feat: 予約のレコードから予約した部屋とユーザーを参照可能に変更

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\Event;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Room extends Model
 {
     use HasFactory;
+
+    public function event()
+    {
+        return $this->hasOne(Event::class);
+    }
 }
